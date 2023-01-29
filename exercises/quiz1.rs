d57b08d54ba37161a6474e69ded4e3ea186dd475
wheelfredie <wilfred.edward.tan@gmail.com>
2023-01-30 00:22:04 +0800

    mac exercises progress update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(number: i16) -> i16{
+    if number > 40 {
+        return number;
+    }
+    else {
+        return number * 2;
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
